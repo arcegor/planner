85b8bdc5c8561673bd04b03620b7a9248ec64fbd
arcegor <yakovlev.polare@gmail.com>
2023-11-22 21:38:48 +0300

    16

--- src/main/java/vkr/planner/model/schedule/ObjectType.java
+++ src/main/java/vkr/planner/model/schedule/ObjectType.java
@@ -1,14 +1,20 @@
 package vkr.planner.model.schedule;
 
 public enum ObjectType {
-    РЕАКТОР("Реактор"),
-    КУХНЯ("Кухня");
+    РЕАКТОР("РЕАКТОР"),
+    КУХНЯ("КУХНЯ");
 
     ObjectType(String description) {
         this.description = description;
     }
+
     public final String description;
-    public ObjectType getByDescription(String description){
-        return ObjectType.valueOf(description);
+    public String getDescription(){
+        return this.description;
+    }
+    public static ObjectType getEnum(String value) {
+        for(ObjectType v : values())
+            if(v.getDescription().equalsIgnoreCase(value)) return v;
+        throw new IllegalArgumentException();
     }
 }
